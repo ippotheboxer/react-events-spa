a229a3d9c1fc7102d432b6bfc32bc1636d7bf1a0
Sophie <sophiesokol123@gmail.com>
2025-03-21 10:33:48 +0000

    Complete project

--- frontend/src/pages/EventsDetail.js
+++ frontend/src/pages/EventsDetail.js
@@ -1,22 +1,31 @@
-import React from 'react';
-import { redirect, useRouteLoaderData } from 'react-router-dom';
+import React, { Suspense } from 'react';
+import { redirect, useRouteLoaderData, Await } from 'react-router-dom';
 import EventItem from "../components/EventItem";
+import EventsList from '../components/EventsList';
 
 const EventsDetail = () => {
-  const data = useRouteLoaderData('event-detail');
+  const { event, events } = useRouteLoaderData('event-detail');
+
   return (
-    <EventItem event={data.event} />
+    <>
+      <Suspense fallback={<p style={{ textAlign: 'center' }}>Loading...</p>}>
+        <Await resolve={event}>
+          {loadedEvent => <EventItem event={loadedEvent} />}
+        </Await>
+      </Suspense>
+      <Suspense fallback={<p style={{ textAlign: 'center' }}>Loading...</p>}>
+        <Await resolve={events}>
+          {loadedEvents => <EventsList events={loadedEvents} />}
+        </Await>
+      </Suspense>
+    </>
   );
 }
 
 export default EventsDetail;
 
-export async function loader({ request, params }) {
-  const id = params.eventId
-
-  const response = await fetch('http://localhost:8080/events/' + id, {
-    method: request.method,
-  });
+async function loadEvent(id) {
+  const response = await fetch('http://localhost:8080/events/' + id);
 
   if (!response.ok) {
     throw new Response(JSON.stringify({
@@ -25,11 +34,32 @@ export async function loader({ request, params }) {
       { status: 500 });
   }
   else {
-    return response;
+    const resData = await response.json();
+    return resData.event;
+  }
+
+}
+
+async function loadEvents() {
+  const response = await fetch('http://localhost:8080/events');
+  if (!response.ok) {
+    throw new Response(JSON.stringify({ message: 'Could not fetch events.' }), { status: 500 });
+  } else {
+    const resData = await response.json();
+    return resData.events;
   }
 }
 
-export async function action({params, request}) {
+export async function loader({ request, params }) {
+  const id = params.eventId
+
+  return {
+    event: await loadEvent(id),
+    events: loadEvents(),
+  }
+}
+
+export async function action({ params, request }) {
   const eventId = params.eventId;
   const response = await fetch('http://localhost:8080/events/' + eventId, {
     method: request.method,
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import PageContent from "../components/PageContent";
 
 const Home = () => {
     return (
-        <div>
-            <h1>Home</h1>
-        </div>
+        <PageContent title="Welcome!">
+            <p>Browse all of our amazing events.</p>
+        </PageContent>
     );
 }
 
